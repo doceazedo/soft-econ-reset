f541a7fc93654d7c3b0a24633c9690292ca5dc41
DoceAzedo <me@doceazedo.com>
2025-01-22 18:01:31 -0300

    check for colored shulker boxes

--- src/main.rs
+++ src/main.rs
@@ -246,7 +246,23 @@ fn process_items_list(items: NbtCompoundList) -> Vec<NbtCompound> {
                     return item.to_owned()
                 }
 
-                "minecraft:shulker_box" => {
+                "minecraft:shulker_box"
+                | "white_shulker_box"
+                | "orange_shulker_box"
+                | "magenta_shulker_box"
+                | "light_blue_shulker_box"
+                | "yellow_shulker_box"
+                | "lime_shulker_box"
+                | "pink_shulker_box"
+                | "gray_shulker_box"
+                | "silver_shulker_box"
+                | "cyan_shulker_box"
+                | "purple_shulker_box"
+                | "blue_shulker_box"
+                | "brown_shulker_box"
+                | "green_shulker_box"
+                | "red_shulker_box"
+                | "black_shulker_box" => {
                     let maybe_container = item
                         .compound("components")
                         .and_then(|components| components.list("minecraft:container"))
